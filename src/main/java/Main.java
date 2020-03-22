afc7437364a9dad89487c09dce462b6e353bc543
shirleyndou <shirley.ndou@umuzi.org>
2020-03-22 14:43:09 +0200

    sql update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,11 @@ public class Main {
             PreparedStatement stmt = con.prepareStatement("SELECT * FROM Customers");
             ResultSet rs = stmt.executeQuery();
 
+            while(rs.next()){
+                System.out.println(rs.getInt(1) + " | " + rs.getString(2) + " | " + rs.getString(3) + " | " +
+                        rs.getString(4) + " | " + rs.getString(5) + " | "  + rs.getString(6) + " | " + rs.getString(8)+ " | " + rs.getString(9));
+            }
+
         } catch (SQLException e) {
             System.err.println(e);;
         }
